aeb5c90919382c1b3dca5d286ef2696d5dec2416
Lim Jing Rong <jing_rong92@hotmail.com>
2017-01-21 20:34:57 +0800

    Pretty login page

--- public/React/Components/LoginScreen.js
+++ public/React/Components/LoginScreen.js
@@ -16,11 +16,33 @@ export class LoginScreen extends React.Component {
   render() {
     // User is logged in
     return (
-      <div>Login</div>
+      <div className="container">
+        <div id="login">
+          <form action="javascript:void(0);" method="get">
+            <fieldset className="clearfix">
+              <div id='loginFormContainer'>
+                <img height='auto' width='80%' style={{marginLeft:'10%'}} src={"/images/HanzoPixel.png"} />
+                <p><span className="fontawesome-user"></span><input type="text" value="Username" onBlur="if(this.value == '') this.value = 'Username'" onFocus="if(this.value == 'Username') this.value = ''" required/></p>
+                <p style={{marginTop:'25px'}}>
+                  <input type="submit" value="Step into the dojo" style={styleSheet.loginButton}/>
+                </p>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+      </div>
     )
   }
   
   setUsername(usernameInput) {
     this.setState({usernameInput: usernameInput})
+  }
+}
+
+const styleSheet = {
+  loginButton : {
+    position: 'absolute',
+    bottom: '0',
+    width: '100%'
   }
 }
